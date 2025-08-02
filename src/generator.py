600ce77a18eb1e09a223152484af26b5253cee15
SACHIN DATTATRAY MOSAMBE <142896147+SachinMosambe@users.noreply.github.com>
2025-08-02 15:42:39 +0530

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -17,11 +17,15 @@ class Generator:
         )
 
     def format_prompt(self, context, query):
-        return f"""You are a helpful assistant. Use the context to answer the user question.
-        
-        Context: {context}
-        Question: {query}
-        """
+        return  """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer. Answer must be detailed and well explained.
+answer:
+"""
 
     def generate(self, context, query, max_tokens=200):
         prompt = self.format_prompt(context, query)
